fe9af84fbaf7d4be1c4efa7727a36090f0e24d2e
OceanTran999 <100577019+OceanTran999@users.noreply.github.com>
2026-04-05 19:12:16 +0900

    Added coded files

---
+++ Web_Content/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Web_Content
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.searchBox = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.web_display = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.web_display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputBox.Location = new System.Drawing.Point(252, 26);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(744, 22);
+            this.inputBox.TabIndex = 0;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(1002, 15);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(106, 39);
+            this.searchBox.TabIndex = 0;
+            this.searchBox.Text = "Search";
+            this.searchBox.UseVisualStyleBackColor = true;
+            this.searchBox.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please enter an URL";
+            // 
+            // web_display
+            // 
+            this.web_display.AllowExternalDrop = true;
+            this.web_display.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.web_display.CreationProperties = null;
+            this.web_display.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.web_display.Location = new System.Drawing.Point(17, 60);
+            this.web_display.Name = "web_display";
+            this.web_display.Size = new System.Drawing.Size(1091, 504);
+            this.web_display.TabIndex = 4;
+            this.web_display.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.searchBox;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1120, 576);
+            this.Controls.Add(this.web_display);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.web_display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button searchBox;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 web_display;
+    }
+}
+
---
+++ Web_Content/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_Content
+{
+    public partial class Form1 : Form
+    {
+        string targetURL;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+        private string getMainDomain(string url, int index)
+        {
+            string retURL = "";
+
+            while (url[index] != '/')
+            {
+                retURL += url[index];
+                index++;
+            }
+
+            retURL += '/';
+            return retURL;
+        }
+        private async void InitializeAsync()
+        {
+            await web_display.EnsureCoreWebView2Async();
+        }
+        private async void httpRequest(string text)
+        {
+            HttpClient client = new HttpClient();
+
+            using (var resp = await client.GetAsync(text))
+            {
+
+                long? totalBytes = resp.Content.Headers.ContentLength;
+                MessageBox.Show(totalBytes.ToString());
+
+/*                int totalByteRead = 0;
+                int byteRead;*/
+                byte[] buffer = new byte[8192];
+
+                string respMess = await resp.Content.ReadAsStringAsync();
+                /*MessageBox.Show(respMess.Length.ToString());
+                MessageBox.Show(resp.Headers.ToString().Length.ToString());*/
+
+                web_display.NavigateToString(respMess.ToString());
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Check if input is empty
+                if (inputBox.Text == "")
+                    throw new Exception("Input is empty...");
+
+                if (inputBox.Text.StartsWith("http://"))
+                    targetURL = "http://" + getMainDomain(inputBox.Text, 7);
+                else if (inputBox.Text.StartsWith("https://"))
+                    targetURL = "https://" + getMainDomain(inputBox.Text, 8);
+
+                /*MessageBox.Show(targetURL);*/
+
+                httpRequest(inputBox.Text);
+                //outputBox.Text = respMess.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*inputBox.Focus();*/
+        }
+
+        private void web_display_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            // Due to some <a> tag with "href" attribute does not include the full URL that leads to error when requesting, so I will make it back to homepage
+            if (e.Uri.Contains("about:blank"))
+            {
+                MessageBox.Show("Error requesting to URL. Back to homepage...");
+                httpRequest(targetURL);
+            }
+            // Update URL in textbox
+            inputBox.Text = targetURL;
+        }
+}
+}
