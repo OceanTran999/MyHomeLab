e5478dbe98e44686b290bea726cfc439c72eb17b
OceanTran999 <100577019+OceanTran999@users.noreply.github.com>
2026-05-02 21:00:04 +0900

    Add files via upload

---
+++ Chat_Message/Socket_Lib/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Server_Chat_Message.Client;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Server_Chat_Message
+{
+    public partial class Form1 : Form
+    {
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClientChat clientUI = new ClientChat();
+            clientUI.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            adminLogin adminUI = new adminLogin();
+            adminUI.Show();
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
